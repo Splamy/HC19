78d1759a8fed644ed1cc44725d3db217cf5ab657
Splamy <splamyn@gmail.com>
2019-02-28 19:09:40 +0100

    parse

--- HC19/Program.cs
+++ HC19/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace HC19
 {
 	class Program
 	{
+		static string[] files = new[] { "a_example.txt" };
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			foreach (var file in files)
+			{
+				ProcessFile( "../../../../Input/" + file);
+			}
+		}
+
+		static Data ProcessFile(string file)
+		{
+			var tags = new Dictionary<string, int>();
+			using (var fs = File.OpenRead(file))
+			using (var s = new StreamReader(fs))
+			{
+				int num = int.Parse(s.ReadLine());
+				var data = new List<Img>(num);
+				for (int i = 0; i < num; i++)
+				{
+					var line = s.ReadLine();
+					var split = line.Split();
+					var itags = new int[int.Parse(split[1])];
+
+					for (int j = 0; j < itags.Length; j++)
+					{
+						if(!tags.TryGetValue(split[j + 2], out var itag))
+						{
+							itag = tags.Count;
+							tags.Add(split[j + 2], itag);
+						}
+						itags[j] = itag;
+					}
+
+					Array.Sort(itags);
+					data.Add(new Img(split[0] == "H", itags));
+				}
+
+				return new Data() { Imgs = data };
+			}
+		}
+	}
+
+	class Data
+	{
+		public List<Img> Imgs;
+	}
+
+	readonly struct Img
+	{
+		public readonly bool H;
+		public readonly int[] Tags;
+
+		public Img(bool h, int[] tags)
+		{
+			H = h;
+			Tags = tags;
 		}
 	}
 }
